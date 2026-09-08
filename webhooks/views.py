823765550387d33bf2e542ff40811fb0c3fdb401
Meiryanne Martins <meirympereira@gmail.com>
2026-09-08 12:30:56 -0300

    feat(webhook): process order events and send outflow notifications

--- webhooks/views.py
+++ webhooks/views.py
@@ -1,6 +1,8 @@
 from rest_framework import views, response, status
 import json
 from webhooks.models import Webhook
+from webhooks.messages import outflow_message
+from services.callmebot import CallMeBot
 
 class WebhookOrderView(views.APIView):
     def post(self, request):
@@ -10,6 +12,23 @@ class WebhookOrderView(views.APIView):
             event_type=data.get('event_type'),
             event=json.dumps(data, ensure_ascii=False),
         )
+
+        product_name = data.get('product')
+        quantity = data.get('quantity')
+        product_cost_price = data.get('product_cost_price')
+        product_selling_price = data.get('product_selling_price')
+        total_value = product_selling_price * quantity
+        profit = total_value - (product_cost_price * quantity)
+
+        message = outflow_message.format(
+            product_name,
+            quantity,
+            total_value,
+            profit,
+        )
+
+        call_me_bot = CallMeBot()
+        call_me_bot.send_message(message)
 
         return response.Response(
             data = data,
